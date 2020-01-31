f957e3fa33ec4dbc7704ab06dd55692675d6cbc8
Justusmays <anthony.mays1984@gmail.com>
2020-01-31 18:48:47 +0000

    initial commit

--- DataStructures.py
+++ DataStructures.py
@@ -1,5 +1,4 @@
 class Queue:
-
     def __init__(self, mode, current_queue=[]):
         self._queue = current_queue
         # depending on the _mode, the queue has to behave like a FIFO or LIFO
@@ -16,3 +15,36 @@ class Queue:
         pass
     def size(self):
         return len(self._queue) 
+
+
+class Justis:
+    def __init__(self, x):
+        self.x = x
+        self.height = "177cm"
+        self.weight = "65kg"
+        self.build = "Slim"
+        self.hair = "black"
+        self.eyes = "brown"
+        self.ethnicity = "black"
+    def print_var(self):
+        print(self.x)
+        print(self.height)
+        print(self.weight)
+        print(self.build)
+        print(self.hair)
+        print(self.eyes)
+        print(self.ethnicity)
+
+
+
+Profile = Justis('Profile')
+Profile.print_var()
+
+
+
+
+
+# print(Profile.height)
+# print(Profile.weight)
+
+
